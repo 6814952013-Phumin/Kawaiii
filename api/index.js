fc0206b194f95a833da887587b0cd15c2c2207b5
6814952013-Phumin <6814952013@rbru.ac.th>
2026-09-24 12:30:17 +0700

    Fix Vercel serverless dependency resolution

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../server/.env") });
-const app = require("../server/src/app");
-const connectDB = require("../server/src/config/db");
 
 let databaseConnection;
+let app;
+let connectDB;
 
 module.exports = async (req, res) => {
     try {
+        app ||= require("../server/src/app");
+        connectDB ||= require("../server/src/config/db");
         databaseConnection ||= connectDB();
         await databaseConnection;
         return app(req, res);
